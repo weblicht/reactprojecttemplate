23b92bd6186dd191128ceced6b6660e2783a17ef
Richard Lawrence <richard.lawrence@uni-tuebingen.de>
2020-08-17 17:30:31 +0200

    Jobs: component: start re-implementing with pure function components
    Move away from class based components where they aren't necessary.
    Relying on generic components from germanet-common to keep things clean.

--- webui/src/Jobs/component.jsx
+++ webui/src/Jobs/component.jsx
@@ -3,117 +3,117 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import { createJob } from './actions';
+import { runJob } from './actions';
+import { selectJob, selectAllJobs } from './selectors';
 
-class JobComp extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+import { Button, Card, List, ListItem } from '@sfstuebingen/germanet-common/components';
+import classNames from 'classnames';
 
-    renderNotFound() {
-        return (
-            <div>
-                <h1>Job ?</h1>
-                <p>Job Not Found</p>
-            </div>
-        );
-    }
+// Displays a card with information about a single job.
+// props:
+//   data: Object representing a job
+function JobAsCard(props) {
+    const statusBadge = props.data.tokens
+          ? <span className="badge badge-success">Completed</span>
+          : (props.data.error
+             ? <span className="badge badge-danger">Error</span>
+             : <span className="badge badge-warning">Processing...</span>);
 
-    render() {
-        // get the jobId from the router or from passed props
-        const id = this.props.match.params.id || this.props.jobId || {};
-
-        if (!this.props.jobs || !id) {
-            return this.renderNotFound();
-        }
-
-        const job = this.props.jobs.find(j => j.id == id);
-        if (!job) {
-            return this.renderNotFound();
-        }
-
-        let statusReport = "";
-        if (job.status === 'done') {
-            statusReport = 
-            <div>
-                <p className="card-text">Untokenized: {job.tokenizedText}</p>
-                <span className="badge badge-pill badge-success">{job.status}</span>
-            </div>
-        } else {
-            statusReport = 
-            <div>
-                <span className="badge badge-pill badge-warning">{job.status}</span>
-            </div>
-        }
-
-        return (
-            <div className="card">
-                <div className="card-body">
-                    {this.props.linkTo
-                    ? <Link to={this.props.linkTo}><h5 className="card-title">Job {job.id}</h5></Link>
-                    : <h5 className="card-title">Job {job.id}</h5>
-                    }
-                    <p className="card-text">Original text: {job.originalText}</p>
-                    {statusReport}
-                </div>
-            </div>
-        );
-    }
-}
-
-JobComp.propTypes = {
-    jobs: PropTypes.array,
-    jobId: PropTypes.number, // optional, in case jobId is not determined by the router
-    linkTo: PropTypes.string // optional
-};
-
-const jobStateToProps = (state) => ({jobs: state.jobs});
-
-export const Job = withRouter(connect(jobStateToProps)(JobComp));
-
-
-class BrowseJobsComp extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-   renderJob(job) {
+         
     return (
-        <Job key={job.id} jobId={job.id} linkTo={"/jobs/" + job.id}/>
+        <Card title={"Job " + props.data.id} level={5}>
+          {statusBadge}
+          <h6>Original text</h6>
+          <p>{props.data.originalText}</p>
+
+          {props.data.tokens &&
+           <>
+             <h6>Tokens</h6>
+             <TokensAsList data={props.data.tokens} />
+           </>
+          }
+
+          {props.data.error &&
+           <>
+             <h6>Error details</h6>
+             <p>{props.data.error}</p>
+           </>
+          }
+        </Card>
     );
-    }
-    renderJobList() {
-        return (
-            <div style={{ borderBottom: '1px solid gray', marginBottom: 20 }}>
-                {this.props.jobs.map(this.renderJob)}
-            </div>
-        );
-    }
-
-    render() {
-        return (
-            <div>
-                <h3> Browse Jobs </h3>
-                {this.props.jobs && this.props.jobs.length
-                    ? this.renderJobList()
-                    : <p>No jobs found</p>
-                }
-
-                <Link to="/jobs/new">
-                    <button type="button" className="btn btn-primary">Add Job</button>
-                </Link>
-            </div>
-        );
-    }
 }
 
-const browseJobsStateToProps = (state) => ({jobs: state.jobs});
+// Displays tokens (i.e., the results of a job) as a list.
+// The list is displayed horizontally on screens size medium and up.
+// props:
+//   data: Array of tokens
+function TokensAsList(props) {
+    if (!props.data && props.data.length) return null;
 
-BrowseJobsComp.propTypes = {
-    jobs: PropTypes.array, // can be null
-};
+    return (
+        <List ordered={true} extras='list-group-horizontal-md'>
+          {props.data.map(token => <ListItem key={token} data={token} />)}
+        </List>
+    );
+    
+}
 
-export const BrowseJobs = connect(browseJobsStateToProps)(BrowseJobsComp);
+// Top level component for /jobs/<id>
+// Displays the details about job <id>, or an error message if there is no such job.
+function JobDetail(props) {
+
+    if (!props.data) {
+        return (
+            <Card title="Job not found" level={5}>
+              There is no job with ID {props.match.params.id}
+            </Card>
+        );
+    }
+
+    return (
+        <JobAsCard data={props.data} />
+    );
+}
+
+function jobDetailStateToProps(state, ownProps) {
+    const id = ownProps.match.params.id;
+    return {
+        data: selectJob(state, id)
+    };
+}
+
+JobDetail = withRouter(connect(jobDetailStateToProps)(JobDetail));
+export { JobDetail };
+
+// Top level component for /jobs
+// Displays a title, the list of jobs, and a button to add a new job.
+function BrowseJobs(props) {
+        return (
+            <>
+              <h3>Browse Jobs</h3>
+
+              {props.jobs && props.jobs.length
+               ? props.jobs.map(job => <JobAsCard key={job.id} data={job} />)
+               : <p>No jobs found</p>
+              }
+
+              <Card>
+                <Link to="/jobs/new">
+                  <button type="button" className="btn btn-primary">Add Job</button>
+                </Link>
+              </Card>
+            </>
+        );
+}
+
+function browseJobsStateToProps(state) {
+    return {
+        jobs: selectAllJobs(state)
+    };
+}
+
+BrowseJobs = connect(browseJobsStateToProps)(BrowseJobs);
+export { BrowseJobs };
 
 class CreateJobComp extends React.Component {
     constructor(props) {
@@ -130,7 +130,8 @@ class CreateJobComp extends React.Component {
     }
 
     onCreateJob(e) {
-        this.props.createJob(this.state.text);
+        e.preventDefault();
+        this.props.runJob(this.state.text);
         // send the user to the BrowseJobs page
         // using the history object from react-router-dom
         this.props.history.push('/jobs');
@@ -165,11 +166,11 @@ class CreateJobComp extends React.Component {
 const createJobStateToProps = (state) => ({ jobs: state.jobs });
 
 const createJobDispatchToProps = (dispatch) => ({
-    createJob: (text) => dispatch(createJob(text)),
+    runJob: (text) => dispatch(runJob(text)),
 });
 
 CreateJobComp.propTypes = {
-    createJob: PropTypes.func.isRequired,
+    runJob: PropTypes.func.isRequired,
 };
 
 export const CreateJob = withRouter(connect(createJobStateToProps, createJobDispatchToProps)(CreateJobComp));
