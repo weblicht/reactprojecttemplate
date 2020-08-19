133bcf4f5de5158ecb5361204c38722d1cdcbf18
Richard Lawrence <richard.lawrence@uni-tuebingen.de>
2020-08-19 16:49:36 +0200

    Main: rework components
    I've redone the components here to clean up the code and make it more
    readable.
    
    In particular:
    
    1. I've removed the distinction between e.g. Footer and
    FooterContainer, which doesn't seem to do anything useful.
    
    2. I've used classNames() to compose CSS classes together and
    documented what the different classes do
    
    3. I've abstracted some of the complexity in the NavBar into a
    separate NavBarItem component
    
    4. I've used Cards for the layout, which also fixed the issue of
    needing a fixed padding height for the header and footer.

--- webui/src/Main/component.jsx
+++ webui/src/Main/component.jsx
@@ -1,111 +1,172 @@
+import { rootReducer } from './reducers';
+import { fetchApiInfo } from './actions';
+import { selectVersion } from './selectors';
+import { JobDetail, BrowseJobs, CreateJob } from '../Jobs/component';
+
+import { Button, Card, CardHeader, CardFooter } from '@sfstuebingen/germanet-common/components';
+
 import React from 'react';
-import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import { compose, applyMiddleware, createStore } from 'redux';
 import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link, useLocation } from 'react-router-dom';
+import classNames from 'classnames';
 
-import { rootReducer } from './reducers';
-import { fetchApiInfo } from './actions';
 
-import { JobDetail, BrowseJobs, CreateJob } from '../Jobs/component';
-
-export const Footer = ({ version }) => (
-    <div className="d-flex flex-row justify-content-center fixed-bottom bg-light" >
-        <div className="d-flex flex-column">
-            <div className="d-flex flex-row justify-content-center">
-                v.{version || ""}
-            </div>
-            <div className="d-flex flex-row justify-content-center">
-            <a target="_blank" href="#">Contact</a>
-            </div>
-        </div>
-    </div>
-);
-
-Footer.propTypes = {
-    version: PropTypes.string,
-};
-
-export class Frame extends React.Component {
+// Renders the nav bar, which is displayed in the header of the main content area.
+// On smaller screens, the nav items collapse behind a "hamburger" menu button.
+export class NavBar extends React.Component {
     constructor(props) {
         super(props);
-        this.openNav = this.openNav.bind(this);
+
+        // a flag indicating whether the nav menu is open or closed
+        // (on screens below the breakpoint):
+        this.state = {
+            isOpen: false
+        };
+
+        // NB: in class components like this one, you need a line like
+        // the following for any method in which 'this' should refer
+        // to the component instance (e.g., so you can call this.setState).
+        // This is extra boilerplate that you can avoid by writing components
+        // as pure functions instead of classes.
+        // See: https://reactjs.org/docs/handling-events.html
         this.closeNav = this.closeNav.bind(this);
         this.toggleNav = this.toggleNav.bind(this);
-        this.state = {
-            showCollapsedNavbar: false
-        };
-    }
-
-    openNav(e) {
-        this.setState({
-            showCollapsedNavbar: true
-        });
     }
 
     closeNav(e) {
         this.setState({
-            showCollapsedNavbar: false
+            isOpen: false
         });
     }
 
     toggleNav(e) {
-        this.setState((prevState) => ({
-            showCollapsedNavbar: !prevState.showCollapsedNavbar
+        // NB: note that we pass setState a function here in order to
+        // safely set the new value of the flag based on the old value. See:
+        // https://reactjs.org/docs/react-component.html#setstate
+        this.setState(prevState => ({
+            isOpen: !prevState.isOpen
         }));
     }
 
     render() {
-        const navCollapseClass = this.state.showCollapsedNavbar ? "collapse navbar-collapse text-right show" : "collapse navbar-collapse";
+        // NB: for more on the (fairly complicated) set of CSS classes
+        // that make Bootstrap Navbars work, see:
+        // https://getbootstrap.com/docs/4.3/components/navbar/
+
+        // classes for the main <nav> element:
+        const navClasses = classNames([
+            "navbar", // make this nav a bootstrap nav bar
+            "navbar-expand-sm", // ...which collapses at the "small" breakpoint
+            "navbar-light", // light theme
+            "bg-light" // light background
+        ]);
+        
+        // classes for the collapsible menu of nav items:
+        const menuClasses = classNames({
+            "collapse": true, "navbar-collapse": true, // enable collapsing
+            "show": this.state.isOpen, // show/hide the dropdown menu 
+            "text-right":  this.state.isOpen, // show nav items on the right in the dropdown
+        });
         
         return (
-            <nav className="navbar navbar-expand-lg fixed-top navbar-light bg-light">
-                <Link className="navbar-brand" to='/'>ReactProject</Link>
+            <nav className={navClasses}>
 
-                <button className="navbar-toggler" type="button" data-toggle="collapse" 
-                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
-                        aria-expanded="false" aria-label="Toggle navigation" 
-                        onClick={this.toggleNav}>
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+              {/* A "branding" link that is always displayed slightly larger than nav items,
+                * and never hidden in a menu: */}
+              <Link className="navbar-brand" to='/'>ReactProject</Link>
+
+              {/* The hamburger button displayed when the menu collapses: */}
+              <Button className="navbar-toggler"
+                      data-toggle="collapse" 
+                      data-target="#navbarSupportedContent"
+                      aria-controls="navbarSupportedContent" 
+                      aria-expanded="false"
+                      aria-label="Toggle navigation" 
+                      onClick={this.toggleNav}>
+                <span className="navbar-toggler-icon"></span>
+              </Button>
                 
-                <div id="navbarSupportedContent" className={navCollapseClass}>
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <Link className="nav-link" to='/' onClick={this.closeNav}>Home<span className="sr-only">(current)</span></Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to='/jobs' onClick={this.closeNav}>Jobs</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to='/jobs/new' onClick={this.closeNav}>New</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link disabled" tabIndex="-1" aria-disabled="true" to="/">Disabled</Link>
-                        </li>
-                    </ul>
-                </div>
+              {/* The actual nav items: */}
+              <div id="navbarSupportedContent" className={menuClasses}>
+                <ul className="navbar-nav mr-auto">
+                  <NavBarItem text="Home" target="/" onClick={this.closeNav}/>
+                  <NavBarItem text="Browse Jobs" target="/jobs" onClick={this.closeNav}/>
+                  <NavBarItem text="Create Job" target="/jobs/new" onClick={this.closeNav}/>
+                </ul>
+              </div>
             </nav>
         );
     }
 }
 
-export const Home = () => (
-        <div>
-            <h1>Home page</h1>
-            <p style={{fontSize:20}}>Please go to the Jobs section.</p>
-        </div>
-);
+// Renders an item in the nav bar.
+// props:
+//   text: the text to display in the nav item  
+//   target: the (relative) URL of the nav item's destination
+//   onClick (optional): a callback to call when the nav item is clicked 
+function NavBarItem(props) {
+    const location = useLocation();
 
-const mapStateToFooterProps = (state) => state.apiinfo;
-export const FooterContainer = connect(mapStateToFooterProps)(Footer);
+    const liClasses = classNames({
+        'nav-item': true, // mark this item as a nav item 
+        'active': location.pathname === props.target // mark as active depending on URL
+    });
 
-const mapStateToHomeProps = (state) => ({});
-export const HomeContainer = connect(mapStateToHomeProps)(Home);
+    const linkClasses = classNames([
+        'nav-link'
+    ]);
 
+    return (
+        <li key={props.target} className={liClasses}>
+          <Link to={props.target} onClick={props.onClick} className={linkClasses}>
+            {props.text}
+          </Link>
+        </li>
+    );
+}
+
+
+// The component displayed at /
+function Home(props) {
+    return (
+        <Card title="Home page" level={3}>
+            <p>Please go to the Jobs section.</p>
+        </Card>
+    );
+}
+
+// A fallback component displayed at all unrecognized URLs
+function NotFound(props) {
+    return (
+        <Card title="Not found" level={3}>
+          <p>There's nothing to see here.</p>
+        </Card>
+    );
+}
+
+// Displays the footer in the main content area with version information 
+function Footer(props) {
+    return (
+        <CardFooter extras="text-center">
+          {props.version && <p>Version {props.version}</p>}
+        </CardFooter>
+    );
+}
+
+function stateToFooterProps(state) {
+    return {
+        version: selectVersion(state)
+    };
+}
+
+Footer = connect(stateToFooterProps)(Footer);
+
+
+// Initialize the Redux store with devtools and thunk middleware:
 const devTools = (window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
     || (f => f);
 
@@ -114,36 +175,53 @@ const middleware = compose(
         createLogger()),
     devTools
 );
+
 const store = createStore(rootReducer, middleware);
 
+// The top-level App component. This component is the entry point for
+// the entire application. Its duties are:
+//   - provide the Redux store to other components lower in the hierarchy
+//   - set up the application router
+//   - set up the outermost CSS container and render the header, footer, and content
+//     on every page
 export class App extends React.Component {
     constructor(props) {
         super(props);
+    }
+
+    componentDidMount() {
+        // NB: this method is called by React after the App is
+        // rendered for the first time, so it's a good place to run
+        // one-time configuration that requires talking to the
+        // backend, like getting the API info:
         store.dispatch(fetchApiInfo());
     }
  
     render() {
+
+        const mainContentExtras = classNames(
+            "h-100", // consume all vertical space
+            "p-0", // remove default Card padding
+            "border-0", // remove default Card border
+        );
+
         return (
             <Provider store={store}>
+              <div className="h-100 p-0 container-fluid">
                 <Router>
-                    <div style={{ paddingBottom: 70, paddingTop: 70 }}>
-                        <Route path='/' component={Frame} />
-                        <Switch>
-                            <Route exact path='/jobs' component={BrowseJobs} />
-                            <Route exact path='/jobs/new' component={CreateJob} />
-                            <Route exact path='/jobs/:id' component={JobDetail} />
-                            <Route exact path='/' component={HomeContainer} />
-                            <Route path='*' component={NotFound} />
-                        </Switch>
-                    </div>
+                  <Card extras={mainContentExtras} header={<NavBar />} footer={<Footer />}>
+                    <Switch>
+                      <Route exact path='/jobs' component={BrowseJobs} />
+                      <Route exact path='/jobs/new' component={CreateJob} />
+                      <Route exact path='/jobs/:id' component={JobDetail} />
+                      <Route exact path='/' component={Home} />
+                      <Route path='*' component={NotFound} />
+                    </Switch>
+                  </Card>
                 </Router>
-                <FooterContainer />
+              </div>
             </Provider>
         );
     }
 }
-
-const NotFound = () => (
-    <div>This page is not found!</div>
-);
 
