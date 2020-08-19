e9f89756cab213fba3eb795c6a4299f5978fac2a
Richard Lawrence <richard.lawrence@uni-tuebingen.de>
2020-08-19 12:04:52 +0200

    Delete code and directories superceded by Main/

--- webui/src/components/Home.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const Home = () => (
-        <div>
-            <h1>Home page</h1>
-            <p style={{fontSize:20}}>Please go to the Jobs section.</p>
-        </div>
-);
